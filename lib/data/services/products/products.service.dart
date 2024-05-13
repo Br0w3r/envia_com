b469a8b24c86ad5f9186a9c7a0341a6e25d58b31
Br0w3r <67021587+Br0w3r@users.noreply.github.com>
2024-05-12 23:22:34 -0600

    fix services products

--- lib/data/services/products/products.service.dart
+++ lib/data/services/products/products.service.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-
-import 'package:envia_com/core/values/globals.dart';
 import 'package:envia_com/data/models/entities/products/product.model.dart';
 import 'package:envia_com/data/models/enums/request_method.enum.dart';
 import 'package:envia_com/data/services/base.service.dart';
@@ -9,23 +6,15 @@ import 'package:envia_com/data/services/products/products.contract.dart';
 class ProductsApiService extends BaseService implements IProductsService {
   @override
   Future<List<Product>> getProduts() async {
-    var jsonResponse = Globals.jsonResponse;
-
-    var decodedJson = json.decode(jsonResponse);
-
-    var productModel = ProductModel.fromJson(decodedJson);
-
-    return productModel.products ?? [];
-
     // Send Token
-    // var jsonTokenResponse = await provider.request<ProductModel>(
-    //   RequestMethod.get,
-    //   '${apiHost.apiUrl}products',
-    // );
+    var jsonTokenResponse = await provider.request<ProductModel>(
+      RequestMethod.get,
+      '${apiHost.apiUrl}products',
+    );
 
-    // var contentResponse = productModelFromJson(jsonTokenResponse);
+    var contentResponse = productModelFromJson(jsonTokenResponse);
 
-    // return contentResponse.products!;
+    return contentResponse.products!;
   }
 
   @override
